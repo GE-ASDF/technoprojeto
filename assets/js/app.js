abb9a9c9e09fce4ddf565b85a64543b784c73ea9
Anderson Souza <andersonsouza007@live.com>
2022-08-18 21:53:29 -0300

    Inserção de animações

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,12 +31,12 @@ const vm = new Vue({
     
     methods:{
         fetchProdutos(){
-            fetch("../../api/produtos.json")
+            fetch("./api/produtos.json")
             .then(r => r.json())
             .then(r=> this.produtos = r);
         },
         fetchProduto(id){
-            fetch(`../../api/produtos/${id}/dados.json`)
+            fetch(`./api/produtos/${id}/dados.json`)
             .then(r=>r.json())
             .then(r=> this.produto = r);
         },
